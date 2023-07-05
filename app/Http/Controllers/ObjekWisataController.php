3fc8978544528f67b16b88bb781beb4f7d23873d
FaresAriandha <AkunKoding2009@gmail.com>
2023-07-05 07:59:39 +0700

    Routing Changes

--- app/Http/Controllers/ObjekWisataController.php
+++ app/Http/Controllers/ObjekWisataController.php
@@ -27,45 +27,12 @@ class ObjekWisataController extends Controller
         ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(ObjekWisataRequest $objekWisataRequest)
-    {
-        $filename = '';
-        $validated = $objekWisataRequest->validated();
-        $wisata_exist = ObjekWisata::where('nama', $validated['nama'])->where('kab_kota', $validated['kab_kota'])->where('provinsi', $validated['provinsi'])->first();
-
-        if ($wisata_exist) {
-            return response()->json([
-                "status" => 400,
-                "messages" => "Objek wisata has already taken",
-            ], 400);
-        }
-
-        foreach ($objekWisataRequest['foto'] as $file) {
-            $filename .= Storage::disk('objek_wisata')->put('', $file);
-            if (array_search($file, $objekWisataRequest['foto']) != count($objekWisataRequest['foto']) - 1) {
-                $filename .= ' | ';
-            }
-        }
-
-        $validated['foto'] = $filename;
-
-        ObjekWisata::create($validated);
-        return response()->json([
-            "status" => 201,
-            "messages" => "Objek wisata " . $validated['nama'] . " berhasil ditambah",
-        ], 201);
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show($id)
     {
-        $wisata = ObjekWisata::all(['id', 'nama', 'deskripsi', 'alamat_lengkap', 'kab_kota', 'provinsi', 'fasilitas', 'foto'])->where('id', $id)->first();
+        $wisata = ObjekWisata::all(['id', 'nama', 'deskripsi', 'alamat_lengkap', 'kab_kota', 'provinsi', 'fasilitas', 'foto', 'jumlah_like', 'jumlah_komen'])->where('id', $id)->first();
         $wisata['foto'] = explode(' | ', $wisata['foto']);
+        $wisata['comments'] = $wisata->comments;
+        $wisata['fasilitas'] = explode(',', $wisata['fasilitas']);
         if (!$wisata) {
             return response()->json([
                 "status" => 404,
@@ -79,81 +46,7 @@ class ObjekWisataController extends Controller
         ], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update($id, ObjekWisataRequest $objekWisataRequest)
+    public function update()
     {
-        //
-        $filename = "";
-        $validated = $objekWisataRequest->validated();
-        $wisata_exist = ObjekWisata::where('id', $id)->first();
-        if (!$wisata_exist) {
-            return response()->json([
-                "status" => 400,
-                "messages" => "Objek wisata not found",
-            ], 400);
-        }
-
-        $another_wisata_exist = ObjekWisata::where('nama', $validated['nama'])->where('kab_kota', $validated['kab_kota'])->where('provinsi', $validated['provinsi'])->whereNot('id', $id)->first();
-
-        if ($another_wisata_exist) {
-            return response()->json([
-                "status" => 400,
-                "messages" => "Objek wisata has already been taken",
-            ], 400);
-        }
-
-        $files_exist = explode(' | ', $wisata_exist['foto']);
-        foreach ($files_exist as $file) {
-            Storage::disk('objek_wisata')->delete($file);
-        }
-
-        foreach ($objekWisataRequest['foto'] as $file) {
-            $filename .= Storage::disk('objek_wisata')->put('', $file);
-            if (array_search($file, $objekWisataRequest['foto']) != count($objekWisataRequest['foto']) - 1) {
-                $filename .= ' | ';
-            }
-        }
-
-        $validated['foto'] = $filename;
-        $wisata_exist->update($validated);
-        return response()->json([
-            "status" => 201,
-            "messages" => "Objek wisata $wisata_exist->nama berhasil diupdate",
-        ], 201);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        //
-        $wisata_exist = ObjekWisata::where('id', $id)->first();
-        if (!$wisata_exist) {
-            return response()->json([
-                "status" => 400,
-                "messages" => "Objek wisata not found",
-            ], 400);
-        }
-
-        $files_exist = explode(' | ', $wisata_exist['foto']);
-        foreach ($files_exist as $file) {
-            Storage::disk('objek_wisata')->delete($file);
-        }
-
-        $deleted = ObjekWisata::where('id', $wisata_exist->id)->delete();
-        if (!$deleted) {
-            return response()->json([
-                "status" => 400,
-                "messages" => "There is problems!"
-            ], 400);
-        }
-
-        return response()->json([
-            "status" => 200,
-            "messages" => "Objek wisata $wisata_exist->nama berhasil dihapus",
-        ], 200);
     }
 }
---
+++ app/Models/KomentarObjekWisata.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KomentarObjekWisata extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+    protected $table = "komentar_objek_wisata";
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function objek_wisata()
+    {
+        return $this->belongsTo(ObjekWisata::class, 'id_objek_wisata');
+    }
+}
